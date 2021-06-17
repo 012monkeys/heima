4fc447ed5a75bdc0685808474b869cf72d633c16
xiaoxiao <8249009+yeahyeah001@user.noreply.gitee.com>
2021-06-17 08:21:55 +0800

    'log'

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,15 @@
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: [
     // 开启路由器懒加载插件
-    '@babel/plugin-syntax-dynamic-import'
+    '@babel/plugin-syntax-dynamic-import',
+    ...prodPlugins
   ]
 }
